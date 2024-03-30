1da4315d1e03fb8c25a945c7e2c4be855a59d542
SusannaMj <141831885+SusannaMj@users.noreply.github.com>
2024-03-30 20:42:23 +1000

    added basic form handling and Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-alert("Hello");
+function generateJoke(event) {
+  event.preventDefault();
+  event.stopImmediatePropagation();
+  new Typewriter("#joke-window", {
+    strings: "Joke comes here...",
+    autoStart: true,
+  });
+}
+
+let jokeForm = document.querySelector("#joke-generator-form");
+jokeForm.addEventListener("submit", generateJoke);
